45b1bff84290512a76b67b6a9f82a1754723e71b
Dani Kalouche <tug39573@temple.edu>
2019-09-30 14:34:21 -0400

    added isWeekend function logic to check weekend impl. Should return true if it is a saturday or sunday.

--- src/paystation/domain/CheckWeekendImpl.java
+++ src/paystation/domain/CheckWeekendImpl.java
@@ -2,6 +2,8 @@ package paystation.domain;
 
 import java.util.Calendar;
 import java.util.Date;
+//to get day of week
+import java.util.GregorianCalendar;
 /**
  *
  * @author dani
@@ -10,7 +12,14 @@ public class CheckWeekendImpl implements CheckWeekend {
     
     @Override
     public boolean isWeekend(){
-        return true;
+        Date date = new Date();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        
+        //get day of week
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        //return true if weekend
+        return day == Calendar.SATURDAY || day == Calendar.SUNDAY;
     }
     
 }
