85574007c0dd56285b1f7afa5453605758a58dd6
sp4business <sp4business@gmail.com>
2019-09-29 20:16:51 -0400

    Created Menu for main and applied a skeleton with test functionality for all functions. The user should be able to select any number and navigate through Paystation. More to come.

--- src/paystation/domain/main.java
+++ src/paystation/domain/main.java
@@ -5,6 +5,45 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
+        
+        boolean menu = true;
+        
+        while(menu == true) {
+            int choice;
+            System.out.println(
+                "Main Menu: Enter a digit for requested action.\n" +
+                "1 = Deposit Coins\n" +
+                "2 = Display\n" +
+                "3 = Buy Ticket\n" +
+                "4 = Cancel\n" +
+                "5 = Change Rate Strategy\n" 
+            );
+            Scanner scan = new Scanner(System.in);
+            choice = scan.nextInt();
+            
+            switch(choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5: 
+                    
+                    break;
+                default: 
+                    break;
+            }
+            menu = false;
+        }
+        System.out.println("Thank you for using our Paystation! Come again!");
+        
     }
     
 }
